1752249e3cdbfb80433884a04c791bfbe9c1d942
RomanYeryomenko <romanopulos76@gmail.com>
2016-12-13 21:31:11 +0200

    lecture1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,60 @@
-window.jQuery
 "use strict";
 //alert("hello world");
 //console.log("hello world");
 //debugger;
 //var x = 'My x';
-jQuery = 'My x';
-x = "My new x";
+//x = "My new x";
+//var x = "";
+
+//console.log(
+//typeof x // "string"
+//);
+
+//var x = 123;
+//console.log(typeof x); // "number";
+
+//var x = true;
+//console.log(typeof x); // "boolean";
+
+//var x = null;
+//console.log(typeof x); // "object";
+
+//var x = undefined;
+//console.log(typeof x); // "undefined";
+
+// var x = {
+	// key1: "value1",
+	// key2: "value2",
+	// key3: "value3"
+// };
+// x.key1 = "new value";
+// x{"key2"} = "new value 2";
+// console.log(typeof x); "object";
+
+//var x = [];
+//console.log(typeof x); // "object";
+
+// var x = ["element1", "element2", "element3", 25, {}, [], null];
+// console.log(x[3]); //25
+// x[3] = 56;
+// console.log(x[3]); //56
+// console.log(typeof x); // "object";
+// x.test = 123;
+// Array. isArray(x); // true;
+
+// var x = function () {};
+// x.key1 = "test";
+// console.log(x.key1); // "test";
+// console.log(typeof x); // "function";
+
+ var x = ["123"];
+ console.log(x); // ["123"];
+ var y = x;
+ console.log(y); // ["123"];
+ 
+ 
+ 
+
+
+
+
